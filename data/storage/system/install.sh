3689785481b85b03ee7310006e9df7bd4638ff37
miragecentury <victorien.vanroye@gmail.com>
2014-07-28 09:08:56 +0200

    - Fix install dépendance

--- data/storage/system/install.sh
+++ data/storage/system/install.sh
@@ -1,5 +1,5 @@
-apt-get install curl
+sudo apt-get install cgroup-lite rc curl p7zip 
 
-~/profondui/system/sched.sh install root
+/root/profondui/system/sched.sh install root
 
 curl -X POST --data 'idmachine=%profond_idmachine%&key=%profond_keymachine%' http://%profond_url%/isready
